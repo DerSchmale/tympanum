973b58593b7aab223295b9d8941b04990d89fbe8
David Lenaerts <david.lenaerts@gmail.com>
2020-12-20 12:19:25 +0100

    Update docs

--- build/types/hull/QuickHull.d.ts
+++ build/types/hull/QuickHull.d.ts
@@ -2,9 +2,9 @@ import { Vector } from "../types";
 import { Facet } from "../geom/Geometry";
 /**
  * QuickHull implements the algorithm of the same name, based on the original paper by Barber, Dobkin and Huhdanpaa.
- * We're not interested in 0- or 1-dimensional cases (the latter can simply be sorted points). QuickHull returns a
- * set of indices into the original point list so we can map it to a different original ata set (fe: points may be a
- * mapping for position vectors on some scene graph object).
+ * We're not interested in 0- or 1-dimensional cases (the latter can simply be the extent of the point values).
+ * QuickHull returns a set of indices into the original point list so we can map it to a different original ata set
+ * (fe: points may be a mapping for position vectors on some scene graph object).
  *
  * @author derschmale <http://www.derschmale.com>
  */
